636955bc1ff500d54a3754b9e1ab99d4a492dcef
Keiran Carpen <keirancarpen@gmail.com>
2022-08-28 19:23:19 -0400

    change non imported util functions to private

--- utils/gameUtils.go
+++ utils/gameUtils.go
@@ -16,11 +16,11 @@ func GenerateHands() (defs.PlayerHand, defs.DealerHand) {
 		rand.Seed(time.Now().UnixNano())
 		randomInt := rand.Intn(len(cards))
 
-		randomCard := defs.Card{Symbol: cards[randomInt], Value: ComputeCardValue(cards[randomInt])}
+		randomCard := defs.Card{Symbol: cards[randomInt], Value: computeCardValue(cards[randomInt])}
 		randomCards[i] = randomCard
 	}
 
-	lowerPlayerCard, higherPlayerCard := SortCardsDesc(randomCards[0], randomCards[2], cards)
+	lowerPlayerCard, higherPlayerCard := sortCardsDesc(randomCards[0], randomCards[2], cards)
 
 	playerHand := defs.PlayerHand{Card1: lowerPlayerCard, Card2: higherPlayerCard}
 	dealerHand := defs.DealerHand{ShowingCard: randomCards[1]}
@@ -28,7 +28,7 @@ func GenerateHands() (defs.PlayerHand, defs.DealerHand) {
 	return playerHand, dealerHand
 }
 
-func ComputeCardValue(symbol string) int {
+func computeCardValue(symbol string) int {
 	if symbol == "A" {
 		return 0
 	}
@@ -40,9 +40,9 @@ func ComputeCardValue(symbol string) int {
 	return num
 }
 
-func SortCardsDesc(card1 defs.Card, card2 defs.Card, cards []string) (defs.Card, defs.Card) {
-	card1Idx := IndexOf(cards, card1.Symbol)
-	card2Idx := IndexOf(cards, card2.Symbol)
+func sortCardsDesc(card1 defs.Card, card2 defs.Card, cards []string) (defs.Card, defs.Card) {
+	card1Idx := indexOf(cards, card1.Symbol)
+	card2Idx := indexOf(cards, card2.Symbol)
 
 	if card1Idx <= card2Idx {
 		return card1, card2
@@ -50,7 +50,7 @@ func SortCardsDesc(card1 defs.Card, card2 defs.Card, cards []string) (defs.Card,
 	return card2, card1
 }
 
-func IndexOf(slice []string, target string) int {
+func indexOf(slice []string, target string) int {
 	for idx, v := range slice {
 		if v == target {
 			return idx
