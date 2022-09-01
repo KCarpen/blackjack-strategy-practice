db853927e36fb47ba0b996489a413f1ee9f4ccd8
Keiran Carpen <keirancarpen@gmail.com>
2022-08-31 23:15:55 -0400

    create function to read csv

--- utils/playerActionsUtils.go
+++ utils/playerActionsUtils.go
@@ -5,9 +5,14 @@ import defs "black-strat/definitions"
 type Actions = map[string]string
 
 func GeneratePlayerActionsMap() Actions {
+	readCSVFile()
 	actions := Actions{}
 
 	return actions
+}
+
+func readCSVFile() {
+
 }
 
 func GenerateCorrectPlayerAction(userAnswer string, playerHand *defs.PlayerHand, dealerHand *defs.DealerHand, actions *Actions) string {
