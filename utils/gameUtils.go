9a5d2fa6ea802a96b391301e62b53f3f1707f531
Keiran Carpen <keirancarpen@gmail.com>
2022-09-04 04:55:09 -0400

    make cards a global variable in utils package

--- utils/gameUtils.go
+++ utils/gameUtils.go
@@ -7,9 +7,9 @@ import (
 	"time"
 )
 
-func GenerateHands() (defs.PlayerHand, defs.DealerHand) {
-	cards := []string{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}
+var cards = []string{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}
 
+func GenerateHands() (defs.PlayerHand, defs.DealerHand) {
 	var randomCards [3]defs.Card
 
 	for i := 0; i < 3; i++ {
