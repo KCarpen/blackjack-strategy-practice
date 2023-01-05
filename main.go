6d7f3596fa159e0ba83b12b159dae22a1819dded
Keiran Carpen <keirancarpen@gmail.com>
2023-01-04 19:44:30 -0500

    add option to select a game

--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"black-strat/games"
+	"fmt"
 )
 
 func main() {
-	games.RunBasicStrategy()
+	switch game := selectGame(); game {
+	case 0:
+		games.RunBasicStrategy()
+	case 1:
+		games.RunCountStrategy()
+	default:
+		games.RunBasicStrategy()
+	}
+}
+
+func selectGame() int {
+	var selectedGame int
+
+	fmt.Println("What game would you like to play? (type the number associated with the game)")
+	fmt.Println("0. basic strategy practice")
+	fmt.Println("1. counting cards practice")
+	fmt.Print(">>> ")
+	fmt.Scan(&selectedGame)
+
+	if selectedGame != 0 && selectedGame != 1 {
+		return 0
+	}
+	return selectedGame
 }
